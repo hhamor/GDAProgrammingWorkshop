582b6befbae69dcb2c9ff737be35d349318f2dc1
Denliner <35616129+hhamor@users.noreply.github.com>
2019-02-06 11:54:51 -0800

    Added fading transition script

---
+++ UnityPractice/Assets/Scripts/LoadSceneV2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneV2 : MonoBehaviour
+{
+    SceneFadeV2 fadeScreen;
+    public int sceneNumber;
+
+    void Awake() {
+        fadeScreen = GameObject.FindObjectOfType<SceneFadeV2>();
+    }
+
+    public void Trigger() {
+        Debug.Log("Begin EndScene");
+        fadeScreen.BeginTransition(sceneNumber);
+    }
+}
